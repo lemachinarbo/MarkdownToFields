b0d2c778861ac35a19025e61c0256f4924f95b9e
Lemachi Barno <michael@diptongonante.com>
2026-01-24 11:25:56 -0500

    bump 1.2.6

--- MarkdownToFields.module.php
+++ MarkdownToFields.module.php
@@ -56,7 +56,7 @@ class MarkdownToFields extends WireData implements Module, ConfigurableModule
   {
     return [
       'title' => 'Markdown to fields',
-      'version' => '1.2.5',
+      'version' => '1.2.6',
       'summary' => 'Markdown files as your content source of truth',
       'description' =>
         'Use markdown as your content. Structure it with simple tags, and enjoy the markdown <-> ProcessWire fields sync',
